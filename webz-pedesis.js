185b3ea64d40a47a9be3b3eedb96a89df4710c81
teremterem <toporok@gmail.com>
2015-01-03 11:48:53 +0200

    removed all commented stuff from webz-pedesis.js

--- webz-pedesis.js
+++ webz-pedesis.js
@@ -5,15 +5,7 @@ webzContent({
 	"navLinks": [
 		{"url": "https://www.google.com/", "title": "Google"},
 		{"url": "http://sass-lang.com/documentation/file.SASS_REFERENCE.html", "title": "Sass"},
-		//{"url": "https://github.com/teremterem/teremterem.github.io", "title": "GitHub"},
-		//{"url": "https://github.com/teremterem/todo", "title": "GitHub"},
 		{"url": "https://github.com/teremterem/webz-pedesis", "title": "GitHub"},
-
-		//{"url": "https://github.com/", "title": "GitHub"},
-		//{"url": "http://shouldiprefix.com/", "title": "Prefixes"},
-		// {"url": "http://caniuse.com/", "title": "CanIUse"},
-		// {"url": "http://rivetsjs.com/docs/reference/", "title": "Rivets.js?"},
-		// {"url": "http://fs.to/", "title": "fs.to"},
 	],
 	"articleTitle": "WebZ Platform v0.9 beta (Pedesis)",
 	"articleHeading": "For now let's just say that WebZ Platform is going to be some sort of a Web Experience Development Suite...<br>The idea behind the page you are looking at right now is to eventually become a fully-fledged Offline Web App which would be installed as an extension to Google Chrome in order to replace Chrome's default New Tab Page App and play a role of a la \"Research\" Bookmarking App. This app would allow for multiple \"Researches\" to be maintained per a registered user, each \"Research\" in a form of two columns: the first one would be a list of references to external articles and short summaries under those references (these summaries are to be put there by the user of the app to summarize why this particular article is mentioned in the current \"Research\" and which conclusions does it bring the researcher to) and the second one whould be a collection of button-like transparent panels mentioning what \"source\" websites (domain names, probably their brandings to be displayed as well) were used (in other words, which \"sources\" the links in the first column come from)...",
@@ -27,55 +19,26 @@ webzContent({
 		},{
 			"title": "Offline Apps with Angular and PouchDB",
 			"titleUrl": "http://www.mircozeiss.com/building-offline-applications-with-angularjs-and-pouchdb/",
-			//"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec."
 		},{
 			"title": "Durandal (Website)",
 			"titleUrl": "http://durandaljs.com/get-started.html#mount-rainier",
-			//"titleUrl": "http://durandaljs.com/get-started.html#navigation",
-			//"titleUrl": "http://durandaljs.com/get-started.html#the-shell",
-			//"titleUrl": "http://durandaljs.com/get-started.html",
-			//"titleUrl": "http://durandaljs.com/",
-			//"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec."
 		},{
-		//	"title": "Custom Newtab Page For Chrome",
-		//	"titleUrl": "http://www.instructables.com/id/Developing-a-Custom-Newtab-Page-For-Google-Chrome/",
-		//	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec."
-		//},{
-			//"title": "Durandal vs Angular (2013)",
-			//"titleUrl": "http://www.johnpapa.net/compare-durandal-to-angular-not-knockout-to-angular/",
-			//"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec."
-		//},{
 			"title": "Compose a Website...",
 			"titleUrl": "http://www.webdesign-inspiration.com/",
-			//"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante.",// Etiam bibendum iaculis libero, eget molestie nisl pharetra in.",// In semper consequat est, eu porta velit mollis nec.",
 		},{
 			"title": "Develop a Custom Web Impression...	",
 			"titleUrl": "http://bradfrost.github.io/this-is-responsive/",
-			//"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor.",// Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in.",// In semper consequat est, eu porta velit mollis nec.",
-		//},{
-		//	"title": "CSS: Using Percentages in Background-Image",
-		//	"titleUrl": "http://www.sitepoint.com/css-using-percentages-in-background-image/",
-		//},{
-		//	"title": "CTCO-ISD1...",
-		//	"titleUrl": "http://marzyond-todo.terems.org/",
-		//},{
-		//	"title": "Паразит (FS)",//- Учение о жизни (FS)",
-		//	"titleUrl": "http://fs.to/video/cartoonserials/iM7kE790Vcthqa7QmPqJd6-parazit-uchenije-o-zhizni.html",
+		},{
+			"title": "Instant Sprite...	",
+			"titleUrl": "http://instantsprite.com/",
 		},
 	],
 	"dialogButtons": [
 		{
 			//"text": "<h1 style='width:100%;text-align:center'>Todo</h1><p style='width:100%;text-align:center'>-&nbsp;The Organizer&nbsp;-</p>",
 			"text": "<h1 style='width:100%;text-align:center'>Todo</h1><p style='width:100%;text-align:center'>-&nbsp;Pins&nbsp;-</p>",
-			//"class": "orange-todo-panel",
 			"url": "http://www.pinterest.com/oleksandrteresc/recalling-deviant-times/",
-			//"url": "https://groups.google.com/forum/#!topic/angular/uqjrykEGfSA",
-			//"url": "https://developer.chrome.com/extensions/sandboxingEval",
-			//"url": "http://www.webz.bz/obsolete/todo",
-			//"url": "http://www.instructables.com/id/Developing-a-Custom-Newtab-Page-For-Google-Chrome/",
-			//"url": "https://ctc-customs.atlassian.net/browse/ICA-556",
-			//"url": "https://developer.mozilla.org/en-US/docs/Web/CSS/transform#scale",
-			//"url": "http://marzyond-todo.terems.org/",
+			//"class": "orange-todo-panel",
 		},{
 			"text": "<h1 style='width:100%;text-align:center'>Mail</h1><p style='width:100%;text-align:center'>-&nbsp;Terem's .org&nbsp;-</p>",
 			//"url": "mailto:todo@terems.org",
@@ -91,37 +54,10 @@ webzContent({
 			"url": "http://www.lingvo.ua/ru",
 		},{
 			"text": "<p style='width:100%;text-align:center'>UX</p>",
-			//"url": "https://www.youtube.com/results?lclk=year&search_query=ux+development&filters=year&search_sort=video_date_uploaded",
 			"url": "https://www.youtube.com/results?lclk=year&search_query=ux+development&filters=year",
-			//"url": "https://www.youtube.com/results?search_query=ux+development",
-		//},{
-		//	"text": "<p style='width:100%;text-align:center'>FS.TO</p>",
-		//	"url": "http://fs.to/",
-		//},{
-		//	"text": "<hr><h2 style='width:100%;text-align:center'>Dropbox</h2><hr>",
-		//	"url": "https://www.dropbox.com/home",
-		//},{
-		//	"text": "<hr><h2 style='width:100%;text-align:center;color:rgba(255,255,255,0.5)'>Bookmarks</h2><hr>",
-		//	"url": "#",
-		//},{
-		//	"text": "<p><i><u>настроение</u></i> - это <i><u>душа</u></i>, тогда как <i><u>разум</u></i> - это всего лишь средство связи между <i><u>настроением</u></i> (тоесть <i><u>душой</u></i>) и тем проявлением <i><u>реальности</u></i>, которое ты сам же себе и выбираешь&nbsp;...</p>",
-		//	"url": "//maqota.org/",
 		},
 	],
 	"footerLinkText": "2015 © Terem's .org",
 	"footerLinkUrl": "http://terems.org/",
-	//"footerLinkUrl": "http://teremterem.github.io/",
 
 });
-
-// https://www.youtube.com/watch?v=J4TdHvixBTI
-// https://www.youtube.com/watch?v=pqKb1OGpDjo
-// https://www.youtube.com/watch?v=JCLi6vSIuPE
-
-
-// 22950
-
-// http://instantsprite.com/
-
-// http://www.blog.highub.com/javascript/javascript-library/preload-iframe-using-javascript/
-
