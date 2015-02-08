346553f4c97ae9a4f61636408711b7c10fd25133
teremterem <toporok@gmail.com>
2015-02-08 12:10:20 +0200

    updated page content

--- js/webz-content-padding.js
+++ js/webz-content-padding.js
@@ -1,7 +1,8 @@
-/**
- * @author Oleksandr
- */
 
 	var webzContent = function(content) {
+		
+		if (window.chrome && chrome.runtime && chrome.runtime.id) {
+			content["dialogButtons"][1]["url"] = "http://mail.terems.org/";
+		}
 		window.rivetsContext = {webz: content};
 	};
--- webz-pedesis.js
+++ webz-pedesis.js
@@ -11,34 +11,15 @@ webzContent({
 	"articleTitle": "WebZ Platform v0.9 beta (Pedesis)",
 	"articleHeading":
 
-		"WebZ Platform is going to be a Git-based web development and hosting platform. "+
-		"It's core is called <b>WebZ Engine</b>. WebZ Engine is written in Java and soon enough will become open-source. "+
-		"It will be both accessible for download as well as deployed in a form of a <b>free</b> online <b>WebZ App hosting</b>. "+
-		"WebZ Apps will be JS-based web applications which UX developers will host/serve directly from GitHub - "+
-		"pretty similar to what GitHub Pages allow us to do (WebZ Engine also has adapters for other storages like Dropbox etc. "+
-		"as well as for local file systems).<br>"+
-
-		"<b><a href='http://todo.terems.org/'>Todo (The Organizer)</a></b> will be one example of such WebZ Application. "+
-		"It is going to be our collaboration tool. Apart from that, it will also be an exemplary Offline Web App developed "+
-		"as a part of WebZ suite in order to demonstrate what <b>WebZ Platform</b> is and what it is capable of (Todo's source code "+
-		"will be available under MIT license).<br>"+
-
-		"One part of Todo will exist in a form of an extension to Google Chrome (support of other browsers to be considered "+
-		"as well) in order to replace Chrome's default New Tab Page App and play a role of a bookmarking app.<br>"+
-		"Another part of it will be hosted at <b><a href='http://todo.terems.org/'>http://todo.terems.org</a></b> and, "+
-		"having richer UI, will expose more power to edit content pinned/created by users.<br>"+
-
-		"While doing a \"Research\" with Todo a user will be able to pin articles, make conclusions under pins, they will also "+
-		"see a button-like list of \"source\" web-sites - web resources where the articles pinned to the current \"Research\" "+
-		"are originated (StackOverflow, YouTube etc).<br>"+
-
-		"The concept is being continuously elaborated in more and more details...<br>",
+		"WebZ Platform is going to be a Git-based web hosting platform. It's core is called <b>WebZ Server</b>. WebZ Server "+
+		"is written in Java and soon enough will become open-source (this page is already being served by it).<br>"+
+		"Apart from being deployed as part of WebZ Platform, WebZ Server will also be accessible for download.<br>"+
+		"WebZ Apps are JS-based web applications which UX developers will soon be able to host/serve directly from GitHub - "+
+		"pretty similar to what GitHub Pages allow us to do (WebZ Server also has adapters for other storages like Dropbox etc. "+
+		"as well as for the local file system).",
 
 	"sections": [
 		{
-		//	"title": "My Obsolete Pages (Bootstrap-Based)...",
-		//	"titleUrl": "http://www.webz.bz/obsolete/obsolete/todo",
-		//},{
 			"title": "Minimum Viable Product...",
 			"titleUrl": "https://www.youtube.com/watch?v=jHyU54GhfGs",
 		},{
@@ -48,7 +29,7 @@ webzContent({
 			"title": "Durandal (Website)",
 			"titleUrl": "http://durandaljs.com/get-started.html#mount-rainier",
 		},{
-			"title": "Develop a Custom Web Impression...",
+			"title": "This Is Responsive...",
 			"titleUrl": "http://bradfrost.github.io/this-is-responsive/",
 		},{
 			"title": "Instant Sprite...",
@@ -57,23 +38,19 @@ webzContent({
 	],
 	"dialogButtons": [
 		{
-			//"text": "<h1 style='width:100%;text-align:center'>Todo</h1><p style='width:100%;text-align:center'>-&nbsp;The Organizer&nbsp;-</p>",
-			"text": "<h1 style='width:100%;text-align:center'>Todo</h1><p style='width:100%;text-align:center'>-&nbsp;Pins&nbsp;-</p>",
-			"url": "http://www.pinterest.com/teremterem/pedesis-from-ancient-greek-a-leaping/",
+			"text": "<h1 style='width:100%;text-align:center'>Register</h1><p style='width:100%;text-align:center'>-&nbsp;Comming Soon&nbsp;-</p>",
+			"url": "http://www.pinterest.com/teremterem/pedesis/",//-from-ancient-greek-a-leaping/",
 			//"class": "orange-todo-panel",
 		},{
 			"text": "<h1 style='width:100%;text-align:center'>Mail</h1><p style='width:100%;text-align:center'>-&nbsp;Terem's .org&nbsp;-</p>",
 			"url": "mailto:info@terems.org",
-			//"url": "http://mail.terems.org/",
-		},{
-			"text": "<h3 style='width:100%;text-align:center'>YouTube</h3>",
-			"url": "https://www.youtube.com/",
 		},{
 			"text": "<h3 style='width:100%;text-align:center'>Deviant</h3>",
 			"url": "http://www.deviantart.com/",
 		},{
-			"text": "<p style='width:100%;text-align:center'>UX</p>",
-			"url": "https://www.youtube.com/results?lclk=year&search_query=ux+development&filters=year",
+			"text": "<p style='width:100%;text-align:center'>YouTube</p>",
+			//"url": "https://www.youtube.com/results?lclk=year&search_query=ux+development&filters=year",
+			"url": "https://www.youtube.com/results?search_query=web+design&filters=year&lclk=year",
 		},
 	],
 	"footerLinkText": "2015 © Terem's .org",
