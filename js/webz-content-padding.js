dfb5c4a7d13efcf7bbc171f28273ff38dcb54b8a
teremterem <toporok@gmail.com>
2015-02-11 19:01:44 +0200

    minor change

--- js/webz-content-padding.js
+++ js/webz-content-padding.js
@@ -8,6 +8,19 @@
 			content["navLinks"][0] = {"url": "http://www.lingvo.ua/ru", "title": "Lingvo"};
 			content["navLinks"][2] = {"url": "https://www.youtube.com/results?search_query=web+design&filters=year&lclk=year", "title": "YouTube"};
 			
+			var sections = content["sections"];
+			sections.pop();
+			sections.push({
+						"title": "Miscellaneous (pins)",
+						"titleUrl": "https://www.pinterest.com/teremterem/miscellaneous/",
+					},{
+						"title": "Internal (pins)",
+						"titleUrl": "https://www.pinterest.com/teremterem/internal/",
+					},{
+						"title": "Licensing (pins)",
+						"titleUrl": "https://www.pinterest.com/teremterem/licensing/",
+					});
+			
 			var dialogButtons = content["dialogButtons"];
 			dialogButtons[0] = {
 				"text": "<h1 style='width:100%;text-align:center'>Platform</h1><p style='width:100%;text-align:center'>-&nbsp;Internal Issues&nbsp;-</p>",
