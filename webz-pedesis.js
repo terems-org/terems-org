ebb2642606e913761933130da0588adbc5d0a20f
teremterem <toporok@gmail.com>
2015-02-12 23:50:41 +0200

    minor chnage

--- webz-pedesis.js
+++ webz-pedesis.js
@@ -16,7 +16,7 @@ webzContent({
 		"<a href=\"https://github.com/terems-org/terems.org\">GitHub repo</a>).<br>"+
 		"WebZ Server is an <b>open source</b> project available under "+
 		"<a href=\"http://www.gnu.org/licenses/agpl-3.0.html\"><b>GNU Affero GPL v3.0</b></a> - namely it's a web server.<br>"+
-		"It is written in Java and is designed to serve web pages from various local and external file sources. "+
+		"It is written in Java and is designed to serve web pages from various local and remote file sources. "+
 		"It's <b>full API</b> (namely <a href=\"https://github.com/terems-org/webz-api\"><b>WebZ API</b></a>) "+
 		"is available under <a href=\"http://www.apache.org/licenses/LICENSE-2.0.html\"><b>Apache License v2.0</b></a>.<br>"+
 		"Soon enough WebZ Server will also natively support markdown...",
