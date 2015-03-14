a74eedf98a3171ed3af8147e12b2efd36e93c627
teremterem <toporok@gmail.com>
2015-03-14 21:42:24 +0200

    folder index - is not empty

--- webz-pedesis.js
+++ webz-pedesis.js
@@ -6,54 +6,27 @@ webzContent({
 		{"url": "https://github.com/orgs/terems-org/people", "title": "People"},
 	],
 
-	/*
-	"sections": [
-		{
-			"title": "Markdown (Adam Pritchard)",
-			"titleUrl": "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet",
-		},{
-			"title": "Markdown (John Gruber)",
-			"titleUrl": "http://daringfireball.net/projects/markdown/syntax",
-		},{
-			"title": "PouchDB (website)",
-			"titleUrl": "http://pouchdb.com/",
-		},{
-			"title": "Durandal (website)",
-			"titleUrl": "http://durandaljs.com/get-started.html#mount-rainier",
-		},{
-			"title": "Sass (reference)",
-			"titleUrl": "http://sass-lang.com/documentation/file.SASS_REFERENCE.html",
-		},{
-			"title": "This Is Responsive",
-			"titleUrl": "http://bradfrost.github.io/this-is-responsive/",
-		},{
-			"title": "Instant Sprite",
-			"titleUrl": "http://instantsprite.com/",
-		},
-	],
-	*/
 	"dialogButtons": [
 		{
-			"text": "<h1 style='width:100%;text-align:center'>Hosting</h1><p style='width:100%;text-align:center'>-&nbsp;cooming soon&nbsp;-</p>",
-			"url": "http://todo-poc-terems-org.appspot.com/",
+			"text": "<h1 style='width:100%;text-align:center'>WebZ</h1><p style='width:100%;text-align:center'>-&nbsp;downloads&nbsp;-</p>",
+			"url": "http://ww.webz.bz/",
 			"target": "_blank",
 			//"class": "orange-todo-panel",
 		},{
-			"text": "<h1 style='width:100%;text-align:center'>Mail</h1><p style='width:100%;text-align:center'>-&nbsp;Terem's .org&nbsp;-</p>",
-			"url": "mailto:info@terems.org",
+			"text": "<h1 style='width:100%;text-align:center'>PaaS</h1><p style='width:100%;text-align:center'>-&nbsp;cooming soon&nbsp;-</p>",
+			"url": "http://todo-poc-terems-org.appspot.com/",
 			"target": "_blank",
 		},{
-			"text": "<h2 style='width:100%;text-align:center'><i>Pinterest</i></h2>",
-			"url": "https://www.pinterest.com/terems_org/pedesis-from-ancient-greek-a-leaping/",
-			"rel": "nofollow",
-			"target": "_blank",
-		},{
-			"text": "<p style='width:100%;text-align:center'><i><b>Issues</b></i></p>",
+			"text": "<h2 style='width:100%;text-align:center'>Issues</h2>",
 			"url": "https://github.com/terems-org/webz-server/issues",
 		},{
-			"text": "<p style='width:100%;text-align:center'><i>Twitter</i></p>",
+			"text": "<h3 style='width:100%;text-align:center'>Twitter</h3>",
 			"url": "https://twitter.com/terems_org",
 			"rel": "nofollow",
+			"target": "_blank",
+		},{
+			"text": "<p style='width:100%;text-align:center'>Contact Us</p>",
+			"url": "mailto:info@terems.org",
 			"target": "_blank",
 		}
 	],
