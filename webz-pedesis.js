509fdf7cc8b4ee1ac7139bcb4a07fcc5d78c4a60
teremterem <toporok@gmail.com>
2015-03-14 22:03:44 +0200

    butifying

--- webz-pedesis.js
+++ webz-pedesis.js
@@ -2,31 +2,32 @@ webzContent({
 
 	"navLinks": [
 		{"url": "http://ww.webz.bz/", "title": "WebZ"},
-		{"url": "https://github.com/terems-org", "title": "GitHub"},
-		{"url": "https://github.com/orgs/terems-org/people", "title": "People"},
+		{"url": "https://github.com/terems-org", "title": "GitHub", "target": "_blank"},
+		{"url": "mailto:info@terems.org", "title": "Feedback", "target": "_blank"},
 	],
 
 	"dialogButtons": [
 		{
 			"text": "<h1 style='width:100%;text-align:center'>WebZ</h1><p style='width:100%;text-align:center'>-&nbsp;downloads&nbsp;-</p>",
 			"url": "http://ww.webz.bz/",
-			"target": "_blank",
 			//"class": "orange-todo-panel",
 		},{
 			"text": "<h1 style='width:100%;text-align:center'>PaaS</h1><p style='width:100%;text-align:center'>-&nbsp;cooming soon&nbsp;-</p>",
 			"url": "http://todo-poc-terems-org.appspot.com/",
 			"target": "_blank",
 		},{
-			"text": "<h2 style='width:100%;text-align:center'>Issues</h2>",
-			"url": "https://github.com/terems-org/webz-server/issues",
-		},{
-			"text": "<h3 style='width:100%;text-align:center'>Twitter</h3>",
+			"text": "<h2 style='width:100%;text-align:center'>Twitter<h2>",
 			"url": "https://twitter.com/terems_org",
 			"rel": "nofollow",
 			"target": "_blank",
 		},{
-			"text": "<p style='width:100%;text-align:center'>Contact Us</p>",
-			"url": "mailto:info@terems.org",
+			"text": "<h3 style='width:100%;text-align:center'>Pinterest</h3>",
+			"url": "https://www.pinterest.com/terems_org/",
+			"rel": "nofollow",
+			"target": "_blank",
+		},{
+			"text": "<p style='width:100%;text-align:center'>Issues</p>",
+			"url": "https://github.com/terems-org/webz-server/issues",
 			"target": "_blank",
 		}
 	],
