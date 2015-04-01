2e7abf2e7ff0569a6a845bc14f4be530a9ece80e
Oleksandr Tereschenko <oleksandr@terems.org>
2015-04-02 00:22:06 +0300

    Update webz-pedesis.js

--- webz-pedesis.js
+++ webz-pedesis.js
@@ -15,7 +15,8 @@ webzContent({
 	"dialogButtons": [
 		{
 			"text": "<h1 style='width:100%;text-align:center'>Server.jar</h1><p style='width:100%;text-align:center'>-&nbsp;runnable&nbsp;-</p>",
-			"url": "https://oss.sonatype.org/content/repositories/snapshots/org/terems/webz-server-packager/0.9.1-SNAPSHOT/webz-server-packager-0.9.1-20150319.204812-1.jar",
+			"url": "humans.md",
+			//"url": "https://oss.sonatype.org/content/repositories/snapshots/org/terems/webz-server-packager/0.9.1-SNAPSHOT/webz-server-packager-0.9.1-20150319.204812-1.jar",
 			//"class": "orange-todo-panel",
 		},{
 			"text": "<h1 style='width:100%;text-align:center'>Hosting</h1><p style='width:100%;text-align:center'>-&nbsp;coming soon&nbsp;-</p>",
@@ -24,12 +25,12 @@ webzContent({
 		},{
 			"text": "<h2 style='width:100%;text-align:center'>Twitter<h2>",
 			"url": "https://twitter.com/terems_org",
-			"rel": "nofollow",
+			//"rel": "nofollow",
 			"target": "_blank",
 		},{
 			"text": "<h3 style='width:100%;text-align:center'>Pinterest</h3>",
 			"url": "https://www.pinterest.com/terems_org/",
-			"rel": "nofollow",
+			//"rel": "nofollow",
 			"target": "_blank",
 		},{
 			"text": "<p style='width:100%;text-align:center'>Issues</p>",
