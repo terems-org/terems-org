716cb000267566f2ef83a48c67a3e81a348a258e
teremterem <toporok@gmail.com>
2015-04-21 09:01:41 +0300

    minor improvements

--- webz-js-files/webz-template-adapter.js
+++ webz-js-files/webz-template-adapter.js
@@ -2,10 +2,10 @@
 var indexTemplate = null;
 
 var webzInit = function(webzFiles) {
-	indexTemplate = Handlebars.compile(webzFiles.getFile("index.html").getFileDownloader().getContentAsStringAndClose());
+	indexTemplate = Handlebars.compile(webzFiles.getFile("/index.html").getFileDownloader().getContentAsStringAndClose());
 };
 
-var webzPreparePageContext = function(webzContext) {
+var webzPreparePageContext = function(webzContext, fullUrl) {
 	var currentFile = webzContext.getCurrentFile();
 	return {
 		"WEBZ-ROOT": {"FULL-URI": webzContext.resolveUri(webzContext.getFile("/"))},
