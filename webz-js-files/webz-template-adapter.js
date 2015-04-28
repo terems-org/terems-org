0a85b0f2e1f02e35d3416c764c69d9b672b3f0d6
teremterem <toporok@gmail.com>
2015-04-28 21:17:40 +0300

    working on www.maqota.org

--- webz-js-files/webz-template-adapter.js
+++ webz-js-files/webz-template-adapter.js
@@ -3,6 +3,7 @@ var indexTemplate = null;
 
 var webzInit = function(webzFiles) {
 	indexTemplate = Handlebars.compile(webzFiles.getFile("/index.html").getFileDownloader().getContentAsStringAndClose());
+	// indexTemplate({}); // for some reason this does some kind of template initialization (which otherwise happens upon first pageload)
 };
 
 var webzPreparePageContext = function(webzContext, fullUrl) {
@@ -10,10 +11,11 @@ var webzPreparePageContext = function(webzContext, fullUrl) {
 	return {
 		"WEBZ-ROOT": {"FULL-URI": webzContext.resolveUri(webzContext.getFile("/"))},
 		"WEBZ-FILE": {"NAME": currentFile.getMetadata().getName()},
-		"MAIN-CONTENT": marked(currentFile.getFileDownloader().getContentAsStringAndClose())
+		"MAIN-CONTENT": currentFile.getFileDownloader().getContentAsStringAndClose()
 	};
 };
 
 var webzRenderPage = function(pageContext) {
+	pageContext['MAIN-CONTENT'] = marked(pageContext['MAIN-CONTENT']);
 	return indexTemplate(pageContext);
 };
