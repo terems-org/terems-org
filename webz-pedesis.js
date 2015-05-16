c2a92bdc1270857554fc03f612685a02752afb26
teremterem <toporok@gmail.com>
2015-05-16 15:06:53 +0300

    restored webz-pedesis.js back

---
+++ webz-pedesis.js
@@ -0,0 +1,50 @@
+webzContent({
+
+	"navLinks": [
+		{
+			"url": "https://github.com/terems-org/webz-server",
+			"title": "GitHub",
+		},
+		{
+			"url": "http://www.maqota.org/",
+			//"url": "https://oss.sonatype.org/content/repositories/snapshots/org/terems/webz-war/0.9.1-SNAPSHOT/webz-war-0.9.1-20150319.204737-1.war",
+			"title": "Projects",
+			//"url": "/webz-war.md",
+			//"title": ".war",
+		},
+		{
+			"url": "mailto:info@terems.org",
+			"title": "Email", "target": "_blank",
+		},
+	],
+
+	"dialogButtons": [
+		{
+			"text": "<h1 style='width:100%;text-align:center'>Server.jar</h1><p style='width:100%;text-align:center'>-&nbsp;runnable&nbsp;-</p>",
+			"url": "/server-jar.md",
+			//"url": "https://oss.sonatype.org/content/repositories/snapshots/org/terems/webz-server-packager/0.9.1-SNAPSHOT/webz-server-packager-0.9.1-20150319.204812-1.jar",
+			//"class": "orange-todo-panel",
+		},{
+			"text": "<h1 style='width:100%;text-align:center'>Hosting</h1><p style='width:100%;text-align:center'>-&nbsp;coming soon&nbsp;-</p>",
+			"url": "http://todo-poc-terems-org.appspot.com/",
+			"target": "_blank",
+		},{
+			"text": "<h2 style='width:100%;text-align:center'>Twitter<h2>",
+			"url": "https://twitter.com/terems_org",
+			//"rel": "nofollow",
+			"target": "_blank",
+		},{
+			"text": "<h3 style='width:100%;text-align:center'>Pinterest</h3>",
+			"url": "https://www.pinterest.com/terems_org/",
+			//"rel": "nofollow",
+			"target": "_blank",
+		},{
+			"text": "<p style='width:100%;text-align:center'>Issues</p>",
+			"url": "https://github.com/terems-org/webz-server/issues",
+			"target": "_blank",
+		}
+	],
+	"footerLinkText": "2015 © Terem's .org",
+	"footerLinkUrl": "http://www.terems.org/",
+
+});
