089e749b3a52f4aff5167771ad8cb428c8fe1ca5
teremterem <toporok@gmail.com>
2015-06-11 12:31:12 +0300

    getting closer and closer to 0.9.1 release

--- webz-pedesis.js
+++ webz-pedesis.js
@@ -7,10 +7,7 @@ webzContent({
 		},
 		{
 			"url": "http://www.maqota.org/",
-			//"url": "https://oss.sonatype.org/content/repositories/snapshots/org/terems/webz-war/0.9.1-SNAPSHOT/webz-war-0.9.1-20150319.204737-1.war",
 			"title": "Projects", "target": "_blank",
-			//"url": "/webz-war.md",
-			//"title": ".war",
 		},
 		{
 			"url": "mailto:info@terems.org",
@@ -22,26 +19,18 @@ webzContent({
 		{
 			"text": "<h1 style='width:100%;text-align:center'>Server.jar</h1><p style='width:100%;text-align:center'>-&nbsp;runnable&nbsp;-</p>",
 			"url": "/server-jar.md",
-			//"url": "https://oss.sonatype.org/content/repositories/snapshots/org/terems/webz-server-packager/0.9.1-SNAPSHOT/webz-server-packager-0.9.1-20150319.204812-1.jar",
-			//"class": "orange-todo-panel",
 		},{
-			"text": "<h1 style='width:100%;text-align:center'>Hosting</h1><p style='width:100%;text-align:center'>-&nbsp;coming soon&nbsp;-</p>",
-			"url": "http://ww.webz.bz/",
-			"target": "_blank",
+			"text": "<h1 style='width:100%;text-align:center'>WebZ.war</h1><p style='width:100%;text-align:center'>-&nbsp;deployable&nbsp;-</p>",
+			"url": "/webz-war.md",
 		},{
-			"text": "<h2 style='width:100%;text-align:center'>Twitter<h2>",
-			"url": "https://twitter.com/terems_org",
-			//"rel": "nofollow",
-			"target": "_blank",
+			"text": "<h3 style='width:100%;text-align:center'>Getting Started<h3>",
+			"url": "/README.md",
 		},{
-			"text": "<h3 style='width:100%;text-align:center'>Pinterest</h3>",
-			"url": "https://www.pinterest.com/terems_org/",
-			//"rel": "nofollow",
-			"target": "_blank",
+			"text": "<h3 style='width:100%;text-align:center'>License</h3>",
+			"url": "/LICENSE.md",
 		},{
-			"text": "<p style='width:100%;text-align:center'>Issues</p>",
-			"url": "https://github.com/terems-org/webz-server/issues",
-			"target": "_blank",
+			"text": "<h3 style='width:100%;text-align:center'>3rd party</h3>",
+			"url": "/LICENSE-3RD-PARTY.md",
 		}
 	],
 	"footerLinkText": "2015 © Terem's .org",
